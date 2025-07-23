948368606778fe73e2ebf634c4969a0f6b6bde05
Marvinmartinezsampayo <marvinmartinezsampayo@gmail.com>
2025-07-22 21:28:29 -0500

    Confirmado

--- Negocio/obj/Debug/net8.0/Negocio.AssemblyInfo.cs
+++ Negocio/obj/Debug/net8.0/Negocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Negocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dcb2904759c97ec1dfd5c3c91b661aa0f52133a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7443ed3fd52a7db0f77cacbdb64da2d3106c10d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Negocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Negocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
